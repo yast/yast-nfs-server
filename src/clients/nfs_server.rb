66d5c11e83a27d637de1df87265f57291e5d1822
Stasiek Michalski <hellcp@opensuse.org>
2019-05-31 14:21:30 +0200

    Add metadata into packages

--- src/clients/nfs_server.rb
+++ src/clients/nfs_server.rb
@@ -175,7 +175,7 @@ module Yast
       CheckSyntaxErrors(NfsServer.exports)
 
       Wizard.CreateDialog
-      Wizard.SetDesktopTitleAndIcon("nfs_server")
+      Wizard.SetDesktopTitleAndIcon("org.oprnSUSE.YaST.NFSServer")
 
       result = Sequencer.Run(_Aliases, _Sequence)
 
--- src/include/nfs_server/ui.rb
+++ src/include/nfs_server/ui.rb
@@ -794,7 +794,7 @@ module Yast
       }
 
       Wizard.CreateDialog
-      Wizard.SetDesktopTitleAndIcon("nfs_server")
+      Wizard.SetDesktopTitleAndIcon("org.oprnSUSE.YaST.NFSServer")
 
       ret = Sequencer.Run(_Aliases, _Sequence)
       UI.CloseDialog
